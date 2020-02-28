1f23b6ddf740a0926efb590647b14277089027d7
lucasfonmiranda <lucasfonmiranda@gmail.com>
2020-02-27 21:57:29 -0300

    feat(newSite) start new site

---
+++ precache-manifest.efa1e0c19060facb00e61bce5ce0e9e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27ba71744f889abdffe9",
+    "url": "/static/css/main.3cb0307e.chunk.css"
+  },
+  {
+    "revision": "27ba71744f889abdffe9",
+    "url": "/static/js/main.27ba7174.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "975a28f40bc6cb7746744d17f53a850f",
+    "url": "/index.html"
+  }
+];
