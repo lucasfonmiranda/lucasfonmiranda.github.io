9c59ea0f327cf600be9943d29e9c38e8768eb968
lucasfonmiranda <lucasfonmiranda@gmail.com>
2020-03-02 23:41:15 -0300

    feat(updateSite) modify webpage

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.efa1e0c19060facb00e61bce5ce0e9e4.js"
+  "/precache-manifest.42c8432048c77bcae1037969f318133a.js"
 );
 
 workbox.clientsClaim();
